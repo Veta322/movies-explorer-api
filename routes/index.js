db574335d3c47f0d6ee40f0c281de6d0c2187672
Veta322 <113565656+Veta322@users.noreply.github.com>
2023-06-20 14:09:33 +0300

    Update index.js 1234

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
+const { errors } = require('celebrate');
 const routes = require('express').Router();
-const cors = require('cors');
 const auth = require('../middlewares/auth');
 const { createUser, login } = require('../controllers/users');
 const { validateRegister, validateLogin } = require('../utils/validators');
+const { handleError } = require('../middlewares/errorHandler');
+const NotFound = require('../utils/errors/NotFound');
+const { requestLogger, errorLogger } = require('../middlewares/logger');
 
-routes.use('*', cors());
+routes.use(requestLogger);
+
+routes.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 routes.post('/signup', validateRegister, createUser);
 routes.post('/signin', validateLogin, login);
@@ -13,5 +22,14 @@ routes.use(auth);
 
 routes.use('/users', require('./users'));
 routes.use('/movies', require('./movies'));
+
+
+routes.all('*', (req, res, next) => { next(new NotFound('Запрашиваемый адрес не найден :( ')); });
+
+routes.use(errorLogger);
+
+routes.use(errors());
+
+routes.use(handleError);
 
 module.exports = { routes };
